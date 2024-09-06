1250cc840a6ec37bbd4e7137cb4467333fdcd567
Yahor Hrytskevich <gritskevichegor@gmail.comm>
2024-09-06 13:27:49 +0300

    init

---
+++ src/main/java/com/example/techtask/service/impl/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.techtask.service.impl;
+
+import com.example.techtask.model.Order;
+import com.example.techtask.service.OrderService;
+
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Override
+    public Order findOrder() {
+        return null;
+    }
+
+    @Override
+    public List<Order> findOrders() {
+        return List.of();
+    }
+}
---
+++ src/main/java/com/example/techtask/service/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.techtask.service.impl;
+
+import com.example.techtask.model.User;
+import com.example.techtask.service.UserService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public User findUser() {
+        String query = "select u.id, u.email, u.\"user_status\" from users u " +
+                "inner join orders o on o.user_id = u.id " +
+                "where o.created_at BETWEEN '2003-01-01' AND '2003-12-31' and o.order_status = 'DELIVERED' " +
+                "group by u.id " +
+                "having sum(price*quantity) = (" +
+                "select max(total_sum) from (" +
+                "select sum(price*quantity) as total_sum from orders o " +
+                "where o.created_at BETWEEN '2003-01-01' AND '2003-12-31' and o.order_status = 'DELIVERED' " +
+                "group by o.user_id" +
+                ") as max_result" +
+                ")";
+        return (User) entityManager.createNativeQuery(query, User.class)
+                .getResultList()
+                .stream()
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<User> findUsers() {
+        String query = "select distinct u.id, u.email, u.\"user_status\" from users u " +
+                "inner join orders o on o.user_id = u.id " +
+                "where o.order_status = 'PAID' and o.created_at BETWEEN '2010-01-01' AND '2010-12-31'";
+        return entityManager.createNativeQuery(query, User.class)
+                .getResultList();
+    }
+}
